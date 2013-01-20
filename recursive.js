f0883a2ae762f1170abd61e3645f11412ecac9df
Shad Beard <sbeard@tunewiki.com>
2013-01-20 01:01:55 -0500

    Refactored to put global variables inside closures and immediately invoke the parse methods and the closure methods

--- recursive.js
+++ recursive.js
@@ -1,62 +1,59 @@
 var myObj = {
-	name: 'level 1',
-	layer: {
-		name: 'level 2',
-		layer: {
-			name: 'level 3',
-			layer: {
-				name: 'level 4',
-				layer: {
-					name: 'level 5'
-				}
-			}
-		}
-	}	
+    name: 'level 1',
+    layer: {
+        name: 'level 2',
+        layer: {
+            name: 'level 3',
+            layer: {
+                name: 'level 4',
+                layer: {
+                    name: 'level 5'
+                }
+            }
+        }
+    }
 };
 
 /*
- * This function recursively grabs the name of the 
+ * This function recursively grabs the name of the
  * object and pushes it to the array.  It starts at
  * the top-most level and recurses down each layer
  */
-input1 = [];
-function parse1(object) {
-	var name = object.name;
-	input1.push(name);
-	console.log(name);
-    // document.write('</br>' + input1);
-	if (object.layer) {
-		parse1(object.layer);
-	}
-	return input1;
-}
+(function() {
+    input1 = [];
+    (function parse1(object) {
+        //input1 = [];
+        input1.name = object.name;
+        //var name = object.name;
+        input1.push(input1.name);
+        //console.log(name);
+        document.write('</br>' + input1);
+        if(object.layer) {
+            parse1(object.layer);
+        }
+        return input1;
+    }(myObj))
+}())
+
 
 /*
  * This function first recurses down to the deepest
  * layer and pushes that name on to the array.  As
- * it recurses back up, it pushes the next name on 
+ * it recurses back up, it pushes the next name on
  * to the array.
  */
-input2 = [];
-function parse2(object) {
-	if (object.layer) {
-		parse2(object.layer);
-	}
-	var name = object.name;
-    
-	input2.push(name);
-	console.log(name);
-    // document.write('</br>' + input2);
+(function() {
+    input2 = [];
+    (function parse2(object) {
+        if(object.layer) {
+            parse2(object.layer);
+        }
+        var name = object.name;
 
-	return input2;
-}
-
-var test1 = parse1(myObj);
-console.log('Test 1: ' + test1);
-// document.write('</br>' + JSON.stringify(test1));
-
-var test2 = parse2(myObj);
-console.log('Test 2: ' + test2);
-// document.write('</br>' + JSON.stringify(test2));
-
+        input2.push(name);
+        console.log(name);
+        // document.write('</br>' + input2);
+        return input2;
+    }(myObj))
+}());
 
